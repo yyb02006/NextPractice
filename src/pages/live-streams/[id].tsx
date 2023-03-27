74656b0ac4cd76275593f3c0483caf6547fb8d4a
alwjrqns <yyb02006@gmail.com>
2023-03-27 12:48:17 +0900

    live-streams:id페이지 마크업, CSS작성

---
+++ src/pages/live-streams/[id].tsx
@@ -0,0 +1,87 @@
+import type { NextPage } from 'next';
+
+const LiveStreamDetail: NextPage = () => {
+	return (
+		<div className='bg-[#101010] text-[#fafafa] h-full min-h-screen font-SCoreDream py-12'>
+			<div className='w-full px-4'>
+				<div className='w-full bg-indigo-500 aspect-video rounded-sm'></div>
+				<h3 className='mt-3 font-GmarketSans font-bold text-xl text-gray-200'>
+					우리 스마트폰이 달라졌어요!
+				</h3>
+				<div className='flex justify-between font-normal text-xs text-gray-400'>
+					<span>iphone_15_Pro_owner 님의 라이브</span>
+					<span>2시간째 방송중</span>
+				</div>
+			</div>
+			<div className='mt-16 h-[42vh] overflow-y-scroll scroll-'>
+				<div className='flex items-start gap-2 mt-4 bg-[#1a1a1a] p-4'>
+					<div className='bg-pink-400 w-8 aspect-square rounded-md' />
+					<div>
+						<div className='font-normal text-xs text-gray-400'>Steve Jebs</div>
+						<div className='font-SCoreDream font-light text-sm leading-6 text-gray-300 min-w-5/6'>
+							아이폰의 아름다움을 모르는 당신이 불쌍해요!
+						</div>
+					</div>
+				</div>
+				<div className='flex justify-end items-start gap-2 mt-4 bg-[#1a1a1a] p-4'>
+					<div className='flex flex-col items-end'>
+						<div className='font-normal text-xs text-gray-400'>Von Neumann</div>
+						<div className='font-SCoreDream font-light text-sm leading-6 text-right text-gray-300 min-w-5/6'>
+							갤럭시의 놀라움을 모르는 당신이 백 배는 더!
+						</div>
+					</div>
+					<div className='bg-indigo-500 w-12 aspect-square rounded-md' />
+				</div>
+				<div className='flex items-start gap-2 mt-4 bg-[#1a1a1a] p-4'>
+					<div className='bg-pink-400 w-8 aspect-square rounded-md' />
+					<div>
+						<div className='font-normal text-xs text-gray-400'>Steve Jebs</div>
+						<div className='font-SCoreDream font-light text-sm leading-6 text-gray-300 min-w-5/6'>
+							아이폰의 아름다움을 모르는 당신이 불쌍해요!
+						</div>
+					</div>
+				</div>
+				<div className='flex justify-end items-start gap-2 mt-4 bg-[#1a1a1a] p-4'>
+					<div className='flex flex-col items-end'>
+						<div className='font-normal text-xs text-gray-400'>Von Neumann</div>
+						<div className='font-SCoreDream font-light text-sm leading-6 text-right text-gray-300 min-w-5/6'>
+							갤럭시의 놀라움을 모르는 당신이 백 배는 더!
+						</div>
+					</div>
+					<div className='bg-indigo-500 w-12 aspect-square rounded-md' />
+				</div>
+				<div className='flex items-start gap-2 mt-4 bg-[#1a1a1a] p-4'>
+					<div className='bg-pink-400 w-8 aspect-square rounded-md' />
+					<div>
+						<div className='font-normal text-xs text-gray-400'>Steve Jebs</div>
+						<div className='font-SCoreDream font-light text-sm leading-6 text-gray-300 min-w-5/6'>
+							아이폰의 아름다움을 모르는 당신이 불쌍해요!
+						</div>
+					</div>
+				</div>
+				<div className='flex justify-end items-start gap-2 mt-4 bg-[#1a1a1a] p-4'>
+					<div className='flex flex-col items-end'>
+						<div className='font-normal text-xs text-gray-400'>Von Neumann</div>
+						<div className='font-SCoreDream font-light text-sm leading-6 text-right text-gray-300 min-w-5/6'>
+							갤럭시의 놀라움을 모르는 당신이 백 배는 더!
+						</div>
+					</div>
+					<div className='bg-indigo-500 w-12 aspect-square rounded-md' />
+				</div>
+			</div>
+			<div className='fixed bottom-8 left-0 w-full mx-auto px-4 '>
+				<div className='relative flex items-center '>
+					<input
+						type='text'
+						className='appearance-none shadow-xl shadow-[#101010] pl-5 pr-14 text-gray-600 w-full bg-gray-200 rounded-full border-0 placeholder-gray-400 outline-none focus:border-green-600 focus:ring-[2px] focus:ring-green-600'
+					/>
+					<button className='absolute pl-2 pr-4 h-full flex justify-center items-center text-2xl font-bold text-green-600 right-0 rounded-full focus:outline-none'>
+						<span>&rarr;</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default LiveStreamDetail;
