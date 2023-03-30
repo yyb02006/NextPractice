ff827ea4c255ee957108fa5d0c3d0b7e3aa59e4d
alwjrqns <yyb02006@gmail.com>
2023-03-31 04:02:37 +0900

    handler구현, api/users/enter경로로 들어오는 api요청에 handler 적용

---
+++ src/libs/server/handler.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(
+	method: 'GET' | 'POST' | 'DELETE' | 'PUT',
+	func: (req: NextApiRequest, res: NextApiResponse) => void
+) {
+	return async function (req: NextApiRequest, res: NextApiResponse) {
+		if (req.method != method) {
+			res.status(405).end();
+		} else {
+			try {
+				/**func에 비동기적 요소가 있을 경우 catch로 넘어가면 안되기 때문에 await 사용 */
+				await func(req, res);
+			} catch (err) {
+				console.log(err);
+				res.status(500).json(err);
+			}
+		}
+	};
+}
--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,10 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import client from '@/libs/server/client';
+import handler from '@/libs/server/withHandler';
 
-export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
-) {
+async function handlerAction(req: NextApiRequest, res: NextApiResponse) {
 	console.log(req.body);
-	req.method === 'POST' ? res.status(200).end() : res.status(405).end();
+	res.status(200).end();
 }
+
+export default handler('POST', handlerAction);
